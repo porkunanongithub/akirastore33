59fba360aed2a0163ba4ecca7e4ea2d148cd5f60
porkunanongithub <porkunanon@gmail.com>
2018-08-08 15:29:16 +0700

    akira v1.0.1 fix

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_ecommerce');
+define('DB_NAME', 'sql12250053');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'sql12250053');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'FTYNM2Kzyp');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'sql12.freemysqlhosting.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
